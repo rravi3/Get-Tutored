e4e6982ea9eb052d107983d6f8a20773e08c1882
rravi3 <ravirahul660@gmail.com>
2023-08-22 20:20:19 -0700

    updates

---
+++ server/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+
+# Create a Flask web application
+print(__file__)
+static_dir = str(os.path.abspath(os.path.join(__file__ , "..", "../static")))
+app = Flask(__name__, static_url_path="", static_folder=static_dir)
+app.template_folder = '../scripts/html'
+
+# Define a route and the corresponding function
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(debug=True)
