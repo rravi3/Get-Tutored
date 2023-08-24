bf09d76b3a52c05bba933f8a6871898b50904bc0
rravi3 <ravirahul660@gmail.com>
2023-08-24 15:32:33 -0700

    updates

--- server/server.py
+++ server/server.py
@@ -5,7 +5,7 @@ import os
 print(__file__)
 static_dir = str(os.path.abspath(os.path.join(__file__ , "..", "../docs")))
 app = Flask(__name__, static_url_path="", static_folder=static_dir)
-app.template_folder = '../scripts/html'
+app.template_folder = '../docs'
 
 # Define a route and the corresponding function
 @app.route('/')
