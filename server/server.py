01012c783cf892e669f559f2e73dbefa2d09554f
rravi3 <ravirahul660@gmail.com>
2023-08-24 15:14:46 -0700

    updates

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ import os
 
 # Create a Flask web application
 print(__file__)
-static_dir = str(os.path.abspath(os.path.join(__file__ , "..", "../static")))
+static_dir = str(os.path.abspath(os.path.join(__file__ , "..", "../docs")))
 app = Flask(__name__, static_url_path="", static_folder=static_dir)
 app.template_folder = '../scripts/html'
 
